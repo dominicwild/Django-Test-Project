ad6e255ccc5e77e05753da06c7e5db07bdb4d169
dominicwild <dominicwild@dominicwild.com>
2023-02-15 14:59:57 +0000

    Added error and login functionality to view

--- djangosite/recipe_site/urls.py
+++ djangosite/recipe_site/urls.py
@@ -4,5 +4,5 @@ from recipe_site.views.user_login_view import UserLoginView
 
 app_name = 'recipe_site'
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='index'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
--- djangosite/recipe_site/views/user_login_view.py
+++ djangosite/recipe_site/views/user_login_view.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -8,3 +11,14 @@ from django.views.generic import TemplateView
 class UserLoginView(LoginView):
     template_name = "recipe_site/login.html"
     next = "recipe_site/home.html"
+
+    def post(self, request, **kwargs):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            user = authenticate(request, username=form_data["username"], password=form_data["password"])
+
+            if user is not None:
+                login(request, user)
+
+        return render(request, self.template_name, {"form": form})
